dedc1244027307d3671f66fcf33e3f84b5f10c09
blixondi <101459072+blixondi@users.noreply.github.com>
2023-05-02 21:52:32 +0700

    add drawer and bottomnav

--- app/src/main/java/com/shem/ubayalibrary/view/MainActivity.kt
+++ app/src/main/java/com/shem/ubayalibrary/view/MainActivity.kt
@@ -2,23 +2,35 @@ package com.shem.ubayalibrary.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.shem.ubayalibrary.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
         navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navView = findViewById<NavigationView>(R.id.navView)
+        bottomNav.setupWithNavController(navController)
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
+                || super.onSupportNavigateUp()
     }
 
 }
