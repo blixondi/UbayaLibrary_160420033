c9a3c5db364cbf5da04844d6fad807e2334f3cb1
blixondi <101459072+blixondi@users.noreply.github.com>
2023-05-06 11:30:51 +0700

    add stuff

--- app/src/main/java/com/shem/ubayalibrary/view/HomeFragment.kt
+++ app/src/main/java/com/shem/ubayalibrary/view/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.shem.ubayalibrary.R
 
 
@@ -20,7 +21,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val btnBrowseBook = view.findViewById<Button>(R.id.btnBrowseBook)
+        val btnBrowseThesis = view.findViewById<Button>(R.id.btnBrowseThesis)
+        btnBrowseBook.setOnClickListener {
+        }
     }
 
 }
