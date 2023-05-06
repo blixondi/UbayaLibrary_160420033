9afc750ddd564f7e339b15db86d3e22a968ecff3
blixondi <101459072+blixondi@users.noreply.github.com>
2023-05-06 10:21:59 +0700

    add book and thesis model

---
+++ app/src/main/java/com/shem/ubayalibrary/model/Model.kt
@@ -0,0 +1,31 @@
+package com.shem.ubayalibrary.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Book(
+    @SerializedName("id")
+    val bookId: String?,
+    @SerializedName("judul")
+    val bookTitle: String?,
+    val isbn: String?,
+    val sinopsis: String?,
+    val penulis: String?,
+    @SerializedName("tahun_terbit")
+    val tahunTerbit: String?,
+    val dimensi: String?,
+    val penerbit: String?,
+    val ketersediaan: String?,
+    val gambar: String?
+)
+
+data class Thesis(
+    @SerializedName("id")
+    val thesisId: String?,
+    val penulis: String?,
+    @SerializedName("judul")
+    val thesisTitle: String?,
+    val abstrak: String?,
+    @SerializedName("tahun_terbit")
+    val tahunTerbit: String?,
+    val jenis_pustaka: String?
+)
