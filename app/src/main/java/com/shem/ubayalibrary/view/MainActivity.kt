05545387ca74ab95b004062ece1d408202ef3eb7
blixondi <101459072+blixondi@users.noreply.github.com>
2023-05-08 00:38:56 +0700

    update bottomnav & navigation drawer restriction

--- app/src/main/java/com/shem/ubayalibrary/view/MainActivity.kt
+++ app/src/main/java/com/shem/ubayalibrary/view/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.shem.ubayalibrary.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
@@ -18,7 +19,9 @@ import com.shem.ubayalibrary.R
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
-
+    /* CHANGES HERE */
+    private var toggleMenu: Boolean = false
+    /* CHANGES HERE */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,20 +34,56 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setupWithNavController(navController)
         NavigationUI.setupWithNavController(navView, navController)
 
+        navController.graph.setStartDestination(R.id.itemHome)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
         navController.addOnDestinationChangedListener{navController, d: NavDestination, arguments->
             if(d.id == R.id.loginFragment){
                 bottomNav.visibility = View.GONE
                 navView.visibility = View.GONE
-            } else {
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                supportActionBar?.setDisplayShowHomeEnabled(false)
+                toggle.isDrawerIndicatorEnabled = false
+                toggleMenu = false
+            }
+            else {
                 bottomNav.visibility = View.VISIBLE
                 navView.visibility = View.VISIBLE
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                if(d.id == R.id.itemHome){
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                    toggle.isDrawerIndicatorEnabled = true
+                    toggleMenu = true
+                } else {
+                    supportActionBar?.setDisplayShowHomeEnabled(false)
+                    toggle.isDrawerIndicatorEnabled = false
+                    toggleMenu = false
+                }
             }
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, drawerLayout)
-                || super.onSupportNavigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggleMenu) {
+            if (item.itemId == android.R.id.home) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START)
+                }
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
